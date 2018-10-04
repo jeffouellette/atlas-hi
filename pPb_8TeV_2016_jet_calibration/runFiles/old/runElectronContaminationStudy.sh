41254dc3b134b10bd612fd64d0520b093ef32057
jeff.ouellette <jeffrey.ouellette@colorado.edu>
2018-10-04 16:38:46 -0600

    Deleted old (outdated) code

--- pPb_8TeV_2016_jet_calibration/runFiles/old/runElectronContaminationStudy.sh
+++
@@ -1,8 +0,0 @@
-# Zee sample for period A
-root -l -b -q '../AnalysisCode/electronContaminationStudy.C (-6, true, "user.jeouelle.2.4.30hi.calibcheck.120.mc15_8TeV.361106.ZeeJet.pPb_myOutput.root")'&
-# ... and for period B
-root -l -b -q '../AnalysisCode/electronContaminationStudy.C (-6, false, "user.jeouelle.2.4.30hi.calibcheck.120.mc15_8TeV.361106.ZeeJet.Pbp_myOutput.root")'&
-
-wait
-
-root -l -b -q '../AnalysisCode/electronContaminationStudyHist.C ()'
